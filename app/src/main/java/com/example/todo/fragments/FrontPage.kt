6ebf04db107b44c4b87969603c7afc7a6721c3c6
xtanion <anandshivam54321@gmail.com>
2021-07-29 10:27:00 +0530

    Navigation to other fragments is done but doesn't pass navArgs

--- app/src/main/java/com/example/todo/fragments/FrontPage.kt
+++ app/src/main/java/com/example/todo/fragments/FrontPage.kt
@@ -5,17 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.todo.R
+import com.example.todo.databinding.FragmentFrontpageBinding
 
 
 class FrontPage : Fragment() {
+
+    var _binding:FragmentFrontpageBinding? = null
+    val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frontpage, container, false)
+        _binding = FragmentFrontpageBinding.inflate(layoutInflater,container,false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.mydayCard.setOnClickListener {
+            val action = FrontPageDirections.actionFrontpageToMydayFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 }
--- app/src/main/java/com/example/todo/fragments/MydayFragment.kt
+++ app/src/main/java/com/example/todo/fragments/MydayFragment.kt
@@ -5,18 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todo.R
+import com.example.todo.TodoRVAdapter
+import com.example.todo.TodoViewModel
+import com.example.todo.databinding.FragmentMydayBinding
 
 
 class MydayFragment : Fragment() {
 
+    var _binding:FragmentMydayBinding? = null
+    val binding get() = _binding!!
+    private lateinit var mViewModel: TodoViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_myday, container, false)
+        _binding = FragmentMydayBinding.inflate(layoutInflater,container,false)
+        val view =  binding.root
+        mViewModel = ViewModelProvider(this).get(TodoViewModel::class.java)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val recyclerView = binding.recyclerView
+        val adapter = TodoRVAdapter()
+
+        mViewModel.readToDo()
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.setHasFixedSize(true)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
