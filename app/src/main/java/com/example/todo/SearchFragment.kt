2e366b61ad65c7b44bf685217efd1945aa5ec883
xtanion <anandshivam54321@gmail.com>
2021-09-02 01:08:42 +0530

    Search fragment Modified (Implementation Remaining)

--- app/src/main/java/com/example/todo/SearchFragment.kt
+++ app/src/main/java/com/example/todo/SearchFragment.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.navigation.Navigation
 import com.example.todo.databinding.FragmentAddBinding
 import com.example.todo.databinding.FragmentSearchBinding
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class SearchFragment : Fragment() {
@@ -23,5 +26,15 @@ class SearchFragment : Fragment() {
         _binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
         //this.dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        activity?.findViewById<BottomAppBar>(R.id.bottom_appbar)?.visibility = View.GONE
+        activity?.findViewById<FloatingActionButton>(R.id.floatingActionButtonMain)?.visibility = View.GONE
+
+        binding.searchLayout.setStartIconOnClickListener {
+            val action = SearchFragmentDirections.actionSearchFragmentToMydayFragment()
+            Navigation.findNavController(view).navigate(action)
+        }
     }
 }
